21dafa70fdbd99d4ba36297d035a1056ed4606bb
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-04-12 13:47:31 +0200

    login and signup button function added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,32 @@ import Home from "./components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./components/Checkout";
 import Login from "./components/Login";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { useStateValue } from "./components/StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    //will only run once when the app component load...
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The user is >> ", authUser);
+      if (authUser) {
+        //the user just logged in/the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     //BEM
     <Router>
